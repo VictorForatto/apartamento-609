b18c7073d65f485f9292939742fa8783e4da4a36
Victor Forato Abreu Pereira <36923061+VictorForatto@users.noreply.github.com>
2026-08-14 15:24:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -553,7 +553,7 @@ window.confirmarReserva = confirmarReserva;
   do dispositivo do convidado.
 */
 function iniciarContagem() {
-  const evento = new Date("2026-09-05T13:00:00-03:00");
+  const evento = new Date("2026-09-12T13:00:00-03:00");
 
   function atualizar() {
     const agora = new Date();
